4fc0a879ddafc19c8b12c7278bbf3fd8ec6c67c8
Bipul Chaudhary <bipulchau@gmail.com>
2021-09-27 23:01:49 -0400

    Redirect Short URLs

--- express_server.js
+++ express_server.js
@@ -40,7 +40,12 @@ app.get("/urls/new", (req, res) => {
 
 app.post("/urls", (req, res) => {
   console.log(req.body);  // Log the POST request body to the console
-  res.send(generateRandomString());         // Respond with 'Ok' (we will replace this)
+  //res.send(generateRandomString());         // Respond with 'Ok' (we will replace this)
+  const shortURL = generateRandomString();
+  urlDatabase[shortURL] = req.body.longURL;
+  //res.send(urlDatabase);
+  res.redirect(`/urls/${shortURL}`);
+
 });
 
 
@@ -48,6 +53,13 @@ app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render("urls_show", templateVars);
 });
+
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});
+
+
 
 // app.get("/hello", (req, res) => {
 //   res.send("<html><body>Hello <b>World</b></body></html>\n");

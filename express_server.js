f4dec86249df54f337152d9dc9ca41115eb2f707
Bipul Chaudhary <bipulchau@gmail.com>
2021-09-29 12:42:10 -0400

    Reordered path in express_server and changed locals.username to username in _header

--- express_server.js
+++ express_server.js
@@ -6,9 +6,11 @@ const cookieParser = require('cookie-parser');
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser())
+app.set("view engine", "ejs");
 
 const PORT = 8080; // default port 8080
 
+//Helper function
 function generateRandomString() {
   const strings = "abcdefghijklmnopqrstuvwxyz0123456789";
   let randomString = "";
@@ -19,7 +21,7 @@ function generateRandomString() {
   return randomString;
 }
 
-app.set("view engine", "ejs");
+
 
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
@@ -39,11 +41,6 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
-app.get("/urls/new", (req, res) => {
-  const templateVars = { username: req.cookies["username"] }
-  res.render("urls_new", templateVars);
-});
-
 app.post("/urls", (req, res) => {
   console.log(req.body); // Log the POST request body to the console
   //res.send(generateRandomString());         // Respond with 'Ok' (we will replace this)
@@ -52,6 +49,12 @@ app.post("/urls", (req, res) => {
   //res.send(urlDatabase);
   res.redirect(`/urls/${shortURL}`);
 });
+
+app.get("/urls/new", (req, res) => {
+  const templateVars = { username: req.cookies["username"] }
+  res.render("urls_new", templateVars);
+});
+
 
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = {
@@ -98,8 +101,6 @@ app.post("/logout", (req, res) => {
   res.redirect("/urls");
 });
 
-
-
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
+  console.log(`Tiny app listening on port ${PORT}!`);
 });

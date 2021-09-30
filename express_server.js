e436d6bc62145a8f3eca4be9949e4fbfed0ece0b
Bipul Chaudhary <bipulchau@gmail.com>
2021-09-29 23:03:11 -0400

    Changed urldatabase structure

--- express_server.js
+++ express_server.js
@@ -41,11 +41,23 @@ const checkIfMatched = function (key, value, usersDatabase) {
   }
 
 
-// global urls Database
+// old global urls Database
+// const urlDatabase = {
+//   b2xVn2: "http://www.lighthouselabs.ca",
+//   "9sm5xK": "http://www.google.com",
+// };
+
+//fresh global urls Database
 const urlDatabase = {
-  b2xVn2: "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com",
-};
+  b6UTxQ: {
+      longURL: "https://www.tsn.ca",
+      userID: "aJ48lW"
+  },
+  i3BoGr: {
+      longURL: "https://www.google.ca",
+      userID: "aJ48lW"
+  }
+}; 
 
 // global users Database
 
